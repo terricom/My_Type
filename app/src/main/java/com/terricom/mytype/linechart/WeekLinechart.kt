be618f8ed5a1547f8b3e3431e1c3a6f4768c753f
Terricom <mo0922@gmail.com>
2019-08-29 00:22:10 +0800

    Finish Diary Fragment and XML

--- app/src/main/java/com/terricom/mytype/linechart/WeekLinechart.kt
+++ app/src/main/java/com/terricom/mytype/linechart/WeekLinechart.kt
@@ -1,18 +1,36 @@
 package com.terricom.mytype.linechart
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.terricom.mytype.databinding.LinechartWeekBinding
+import ir.farshid_roohi.linegraph.ChartEntity
 
 class WeekLinechart : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding = LinechartWeekBinding.inflate(inflater)
 
+         val graph1 = floatArrayOf(113000f, 183000f, 188000f, 695000f, 324000f, 230000f, 188000f, 15000f, 126000f, 5000f, 33000f)
+         val graph2 = floatArrayOf(0f, 245000f, 1011000f, 1000f, 0f, 0f, 47000f, 20000f, 12000f, 124400f, 160000f)
+         var legendArr = arrayOf("05/21", "05/22", "05/23", "05/24", "05/25", "05/26", "05/27", "05/28", "05/29", "05/30", "05/31")
+
+        val firstChartEntity = ChartEntity(Color.WHITE, graph1)
+        val secondChartEntity = ChartEntity(Color.YELLOW, graph2)
+
+        val list = ArrayList<ChartEntity>()
+        list.add(firstChartEntity)
+        list.add(secondChartEntity)
+//        lineChart.legendArray?.let {
+//            legendArr = it }
+//        lineChart.setList(list)
+
         return binding.root
     }
+
+
 
 }
